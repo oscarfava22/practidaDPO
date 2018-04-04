dfc29fc7c04e1f681fbcea5e8c6ebb4a6d91cdcd
RamonFarre <ramon.farre.2016@salleurl.edu>
2018-04-05 00:26:49 +0200

    Implementació de la vista corresponent a la carta. Implementació de la vista de cada item. S'ha afegit un icona per la cistella de compra.

--- src/reserva/Main.java
+++ src/reserva/Main.java
@@ -1,9 +1,11 @@
 package reserva;
 
 import reserva.view.AutenticacioView;
+import reserva.view.CartaView;
 
 public class Main {
     public static void main(String[] args) {
-        new AutenticacioView();
+        //new AutenticacioView();
+        new CartaView(5, 10, 4, 3);
     }
 }
--- src/reserva/view/CartaView.java
+++ src/reserva/view/CartaView.java
@@ -1,37 +1,91 @@
 package reserva.view;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class CartaView extends JFrame{
 
-    public CartaView () {
+    public CartaView (int numEntrants, int numPrimers, int numBegudes, int numPostres) {
 
         setSize(800,600);
         setTitle("Carta");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        //North
+
+        JMenuBar jmbNorth = new JMenuBar();
+        jmbNorth.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+
+        JMenu jmNorth = new JMenu();
+        try {
+            Image image = ImageIO.read(new File("data/ic_shopping_basket_black_24dp_2x.png"));
+            Icon icon = new ImageIcon(image);
+            jmNorth.setIcon(icon);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //jmNorth.setIcon((UIManager.getIcon("OptionPane.errorIcon")));
+        jmbNorth.add(jmNorth);
+
+        JMenuItem jmi1 = new JMenuItem("Productos:");
+        JMenuItem jmi2 = new JMenuItem("Pan con tomate - x2");
+        jmNorth.add(jmi1);
+        jmNorth.add(jmi2);
+
+        getContentPane().add(jmbNorth, BorderLayout.PAGE_START);
+
         //Center
 
         JTabbedPane jtpCenter = new JTabbedPane();
 
-        JPanel jpEntrants = new JPanel();
-        jtpCenter.addTab("Entrants", jpEntrants);
-
         //Taula Entrants
 
-        JTable jtEntrants = new JTable();
-        //jtEntrants.ad
+        JPanel jpEntrants2 = new JPanel(new GridLayout(numEntrants,1));
 
-        JPanel jpPrimers = new JPanel();
-        jtpCenter.add("Principals", jpPrimers);
+        for(int i = 0; i < numEntrants; i++) {
+            jpEntrants2.add(new ItemCartaView("Nom entrant", 10));
+        }
 
-        JPanel jpBegudes = new JPanel();
-        jtpCenter.add("Begudes", jpBegudes);
+        JScrollPane jpEntrants = new JScrollPane(jpEntrants2);
+        jtpCenter.addTab("Entrants", jpEntrants);
 
-        JPanel jpPostres = new JPanel();
-        jtpCenter.add("Postres", jpPostres);
+
+        //Taula Primers
+
+        JPanel jpPrimers2 = new JPanel(new GridLayout(numPrimers,1));
+
+        for(int i = 0; i < numPrimers; i++) {
+            jpPrimers2.add(new ItemCartaView("Nom primer", 5));
+        }
+
+        JScrollPane jpPrimers = new JScrollPane(jpPrimers2);
+        jtpCenter.addTab("Principals", jpPrimers);
+
+        //Taula Begudes
+
+        JPanel jpBegudes2 = new JPanel(new GridLayout(numBegudes,1));
+
+        for(int i = 0; i < numBegudes; i++) {
+            jpBegudes2.add(new ItemCartaView("Nom beguda", 10));
+        }
+
+        JScrollPane jpBegudes = new JScrollPane(jpBegudes2);
+        jtpCenter.addTab("Begudes", jpBegudes);
+
+        //Taula Postres
+
+        JPanel jpPostres2 = new JPanel(new GridLayout(numPostres,1));
+
+        for(int i = 0; i < numPostres; i++) {
+            jpPostres2.add(new ItemCartaView("Nom postre", 10));
+        }
+
+        JScrollPane jpPostres = new JScrollPane(jpPostres2);
+        jtpCenter.addTab("Postres", jpPostres);
 
         getContentPane().add(jtpCenter, BorderLayout.CENTER);
 
