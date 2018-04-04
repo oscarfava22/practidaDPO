2c65eaf9ca627629880d1e4c58423ff848f76dc5
Xose LoLo <1a09lopxose@gmail.com>
2018-04-04 11:04:54 +0200

    Inicio de la creación de la vista GestionMesas en clase.

--- src/servidor/view/GestionMesasView.java
+++ src/servidor/view/GestionMesasView.java
@@ -8,28 +8,51 @@ import java.awt.*;
 
 public class GestionMesasView extends JPanel {
 
-    private JLabel jlTitle;
+    private int nMesas;
 
-    public GestionMesasView() {
+    private JPanel jpGeneral;
 
-        setLayout(new BorderLayout());
+    private JScrollPane jspListaMesas;
+    private JLabel[] jlListaMesas;
 
-        jlTitle = new JLabel();
-        jlTitle.setHorizontalAlignment(SwingConstants.CENTER);
+    /**
+     *
+     */
+    public GestionMesasView(int nMesas) {
+        this.nMesas = nMesas;
 
+        jpGeneral = new JPanel();
+        jpGeneral.setLayout(new BorderLayout());
 
+        jspListaMesas = new JScrollPane();
+        jspListaMesas.setLayout(new GridLayout(nMesas, 1));
+        jspListaMesas.setPreferredSize(new Dimension(50, 350));
 
+        jlListaMesas = new JLabel[nMesas];
+        for (int i = 0; i < nMesas; i++){
+            jlListaMesas[i] = new JLabel("MESA " + Integer.toString(i + 1));
+            jlListaMesas[i].setPreferredSize(new Dimension(50, 20));
+        }
 
-        add(jlTitle, BorderLayout.NORTH);
     }
 
+
+    /**
+     * Crea la vista del apartado Gestionar Mesas
+     * @param mainViewModel
+     */
     public void initView(MainViewModel mainViewModel) {
-        jlTitle.setText(mainViewModel.getGestionMesas());
+        //jlTitle.setText(mainViewModel.getGestionMesas());
     }
 
+
+    /**
+     * Registra los controladores para la vista
+     * @param gestionMesasViewListener
+     */
     public void registerControllers(MouseInputListener gestionMesasViewListener) {
 
-        jlTitle.addMouseListener(gestionMesasViewListener);
+        //jlTitle.addMouseListener(gestionMesasViewListener);
 
     }
 }
