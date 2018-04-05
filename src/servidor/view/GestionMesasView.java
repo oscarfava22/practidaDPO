8e5b6378828b46253870bd4d6c7bd1a4eb763020
Xose LoLo <1a09lopxose@gmail.com>
2018-04-05 22:40:19 +0200

    Vista GestionMesas  - scrollpane arreglado  - grid con bordes

--- src/servidor/view/GestionMesasView.java
+++ src/servidor/view/GestionMesasView.java
@@ -8,7 +8,7 @@ import java.awt.*;
 
 public class GestionMesasView extends JPanel {
 
-    public static final int RESERVA_HEIGHT = 75;
+    public static final int MAX_RESERVAS = 40;
 
     private int nMesas;
 
@@ -22,11 +22,12 @@ public class GestionMesasView extends JPanel {
 
     private JPanel jpRight;
         private JLabel jlMesaNumero;
-        private JScrollPane jspReservasMesa;
-            private JPanel jpListaReservasMesa;
-                private JLabel[] jlNombreReserva;
-                private JLabel[] jlHoraReserva;
-                private JLabel[] jlFechaReserva;
+        private JPanel jpReservas;
+            private JScrollPane jspReservasMesa;
+                private JPanel jpListaReservasMesa;
+                    private JLabel[] jlNombresReserva;
+                    private JLabel[] jlHorasReserva;
+                    private JLabel[] jlFechasReserva;
         private JPanel jpBotones;
             private JButton jbEliminarMesa;
             private JButton jbAnadirMesa;
@@ -63,30 +64,55 @@ public class GestionMesasView extends JPanel {
         jpRight.add(jlMesaNumero, BorderLayout.PAGE_START);
 
         //Panel Central
+        jpReservas = new JPanel(new BorderLayout());
         jspReservasMesa = new JScrollPane();
-        jpListaReservasMesa = new JPanel(new GridLayout(1, 3));
+
+        jpListaReservasMesa = new JPanel(new GridLayout(MAX_RESERVAS, 3));
         //jspReservasMesa.setBorder(BorderFactory.createTitledBorder("Reservas"));
         jpListaReservasMesa.setBorder(BorderFactory.createTitledBorder("Reservas"));
         Font titleFont = new Font(getFont().getName(), Font.BOLD, getFont().getSize());
         JLabel jlNombreReserva = new JLabel("Nombre de la Reserva");
+        jlNombreReserva.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.BLACK));
         jlNombreReserva.setHorizontalAlignment(SwingConstants.CENTER);
         jlNombreReserva.setVerticalAlignment(SwingConstants.NORTH);
         jlNombreReserva.setFont(titleFont);
         jpListaReservasMesa.add(jlNombreReserva);
         JLabel jlHoraReserva = new JLabel("Hora (hh:mm)");
+        jlHoraReserva.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.BLACK));
         jlHoraReserva.setHorizontalAlignment(SwingConstants.CENTER);
         jlHoraReserva.setVerticalAlignment(SwingConstants.NORTH);
         jlHoraReserva.setFont(titleFont);
         jpListaReservasMesa.add(jlHoraReserva);
         JLabel jlFechaReserva = new JLabel("Fecha (dd/mm/aaaa)");
+        jlFechaReserva.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.BLACK));
         jlFechaReserva.setHorizontalAlignment(SwingConstants.CENTER);
         jlFechaReserva.setVerticalAlignment(SwingConstants.NORTH);
         jlFechaReserva.setFont(titleFont);
         jpListaReservasMesa.add(jlFechaReserva);
 
-        jspReservasMesa.add(jpListaReservasMesa);
+        jlNombresReserva = new JLabel[MAX_RESERVAS - 1];
+        jlHorasReserva = new JLabel[MAX_RESERVAS - 1];
+        jlFechasReserva = new JLabel[MAX_RESERVAS - 1];
+        for (int i = 0; i < MAX_RESERVAS - 1; i++){
+            jlNombresReserva[i] = new JLabel("EJEMPLO NOMBRE");
+            jlNombresReserva[i].setBorder(BorderFactory.createMatteBorder(0,2,2,0, Color.BLACK));
+            jlNombresReserva[i].setHorizontalAlignment(SwingConstants.CENTER);
+            jpListaReservasMesa.add(jlNombresReserva[i]);
+            jlHorasReserva[i] = new JLabel("EJEMPLO HORA");
+            jlHorasReserva[i].setBorder(BorderFactory.createMatteBorder(0,0,2,0, Color.BLACK));
+            jlHorasReserva[i].setHorizontalAlignment(SwingConstants.CENTER);
+            jpListaReservasMesa.add(jlHorasReserva[i]);
+            jlFechasReserva[i] = new JLabel("EJEMPLO FECHA");
+            jlFechasReserva[i].setBorder(BorderFactory.createMatteBorder(0,0,2,2, Color.BLACK));
+            jlFechasReserva[i].setHorizontalAlignment(SwingConstants.CENTER);
+            jpListaReservasMesa.add(jlFechasReserva[i]);
+        }
+
+        jspReservasMesa = new JScrollPane(jpListaReservasMesa);
+        //jspReservasMesa.add(jpListaReservasMesa);
+        jpReservas.add(jspReservasMesa, BorderLayout.CENTER);
         //jpRight.add(jspListaMesas, BorderLayout.CENTER);
-        jpRight.add(jpListaReservasMesa, BorderLayout.CENTER);
+        jpRight.add(jpReservas, BorderLayout.CENTER);
 
 
         //Botones
